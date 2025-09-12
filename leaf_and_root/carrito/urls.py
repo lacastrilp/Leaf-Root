49d54a81d26bba830c6adb8d057e064b872dde85
Luisa Castrillón <tucorreo@example.com>
2025-09-12 12:46:59 -0500

    Contador del carrito

--- leaf_and_root/carrito/urls.py
+++ leaf_and_root/carrito/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('remove/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('update/<int:product_id>/', views.update_cart_quantity, name='update_cart_quantity'),
     
+    
 
 ]
